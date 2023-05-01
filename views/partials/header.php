e0d7625f72c25b43e435014c3199075ea3cecf15
OlehMaliuta <104268973+OlehMaliuta@users.noreply.github.com>
2023-05-01 22:58:38 +0300

    localization files update and a bug fix
    * added strings to the ru.json localization file;
    * fixed the language drop menu.

--- views/partials/header.php
+++ views/partials/header.php
@@ -8,30 +8,32 @@ use core\Locale;
     <p class="logo">My Blog</p>
     <nav class="main-top-nav">
         <a class="chat-link link" href="/">Blog</a>
-        <? if (Auth::check() && Auth::get()['id'] == 1) : ?>
-            <a class="users-link link" href="/users"><?= Locale::get('header.users') ?></a>
+        <? if (Auth::check() && Auth::get()['id'] == 1): ?>
+            <a class="users-link link" href="/users">
+                <?= Locale::get('header.users') ?>
+            </a>
         <? endif; ?>
     </nav>
     <nav class="extra-top-nav">
         <div class="lang">
             <img class="lang-menu" />
             <div class="drop-content">
-                <p class="lang-var">
-                    <a href="/lang/en">English</a>
-                </p>
-                <p class="lang-var">
-                    <a href="/lang/ua">Українська</a>
-                </p>
-                <p class="lang-var">
-                    <a href="/lang/ru">Русский</a>
-                </p>
+                <a class="lang-var" href="/lang/en">English</a>
+                <a class="lang-var" href="/lang/ua">Українська</a>
+                <a class="lang-var" href="/lang/ru">Русский</a>
             </div>
         </div>
-        <? if (Auth::check()) : ?>
-            <a class="logout-link link" href="/logout"><?= Locale::get('header.logout') ?></a>
-        <? else : ?>
-            <a class="login-link link" href="/login"><?= Locale::get('header.login') ?></a>
-            <a class="reg-link link" href="/registration"><?= Locale::get('header.register') ?></a>
+        <? if (Auth::check()): ?>
+            <a class="logout-link link" href="/logout">
+                <?= Locale::get('header.logout') ?>
+            </a>
+        <? else: ?>
+            <a class="login-link link" href="/login">
+                <?= Locale::get('header.login') ?>
+            </a>
+            <a class="reg-link link" href="/registration">
+                <?= Locale::get('header.register') ?>
+            </a>
         <? endif; ?>
     </nav>
 </header>
